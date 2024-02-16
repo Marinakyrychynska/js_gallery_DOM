478091a639c2a3eff1f37bc7ee551ac6dd3f3f91
Maryna Kyrychynska <m_kirichok@ukr.net>
2024-02-16 16:47:01 +0200

    make build

---
+++ index.d131017d.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-shadow */var e=document.querySelector(".gallery__list"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target;if(!r.classList.contains("gallery__img"))return!1;t.src=r.parentElement.href});//# sourceMappingURL=index.d131017d.js.map
+
+//# sourceMappingURL=index.d131017d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-shadow */ "use strict";
+const list = document.querySelector(".gallery__list");
+const largeImg = document.getElementById("largeImg");
+list.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const item = event.target;
+    if (!item.classList.contains("gallery__img")) return false;
+    largeImg.src = item.parentElement.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
